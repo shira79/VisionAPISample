1848730866c293519ef798f675ab57fda8b312cc
shira <shira79@users.noreply.github.com>
2020-10-04 02:33:06 +0900

    db設定、モデル作成、migration

---
+++ database/migrations/2020_10_03_095956_create_estates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('info');
+            $table->bigInteger('file_id')->unsigned();
+            $table->dateTime('sent_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('file_id')->references('id')->on('files');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estates');
+    }
+}
